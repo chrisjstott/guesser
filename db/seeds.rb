b1a25fa604efb625169a4742943facbe9887e2ac
Chris Stott <chrisjstott@gmail.com>
2015-09-16 10:57:15 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+500.times do
+  height = rand(55..85)
+  weight = rand(135..255)
+  Person.create(height: height, weight: weight, gender: 'M')
+end
+
+500.times do
+  height = rand(50..80)
+  weight = rand(105..225)
+  Person.create(height: height, weight: weight, gender: 'F')
+end
